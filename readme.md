b72b70435ac04de5e5fc59352f091af71ca54ef9
yonh <laiyongheng@yeseyao.com>
2022-03-01 15:57:03 +0800

    init

---
+++ readme.md
@@ -0,0 +1,16 @@
+```shell
+# 查看支持的编译目标系统
+rustc --print target-list
+# 添加编译目标系统支持
+rustup target add thumbv7em-none-eabihf
+# 指定交叉编译目标
+cargo build --target thumbv7em-none-eabihf
+# 在当前目录使用 nightly 版本的 Rust
+rustup override add nightly
+# cargo xbuild 封装了 cargo build
+# 但不同的是，它将自动交叉编译 core 库和一些编译器内建库（compiler built-in libraries）
+cargo install cargo-xbuild
+# 下载rust源码
+rustup component add rust-src
+```
+
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std] // 禁止与标准库链接
+#![no_main] // 要告诉 Rust 编译器我们不使用预定义的入口点，我们可以添加 #![no_main] 属性。
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
+    // 默认命名为 `_start`
+    loop {}
+}
+
+
+/// 这个函数将在 panic 时被调用 
+/// 类型为 PanicInfo 的参数包含了 panic 发生的文件名、代码行数和可选的错误信息。这个函数从不返回，所以他被标记为发散函数（diverging function）。
+/// 发散函数的返回类型称作 Never 类型（“never” type），记为!。对这个函数，我们目前能做的很少，所以我们只需编写一个无限循环 loop {}。
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
